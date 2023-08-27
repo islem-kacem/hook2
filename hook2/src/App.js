4b03bf3569cbf7f7ae246299509823ec5e909d0d
islem kacem <islemkacem2@gmail.com>
2023-08-27 10:59:18 +0100

    final

--- hook2/src/App.js
+++ hook2/src/App.js
@@ -3,6 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import InfoPage from "./Pages/InfoPage";
 import Description from "./Pages/Description";
+import Register from "./Pages/Register";
+import Login from "./Pages/Home";
+
+
 
 function App() {
 
@@ -10,13 +14,15 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element ={<Register/>} />
+        <Route path="/login" element ={<Login/>} />
         <Route path="/info" element={<InfoPage />} />
         <Route path="/decription/:id" element={<Description />} />
       </Routes>
     </div>
+    
   );
 }
 
--- hook2/src/Pages/Description.js
+++ hook2/src/Pages/Description.js
@@ -3,6 +3,7 @@ import React, { useState,useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import {InfoData} from "../Data/InfoData";
 
+
 const Description = () => {
   const [Description,setDescription]=useState({});
   const { id } = useParams();
